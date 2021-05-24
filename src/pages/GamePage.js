72a356e2bb0efafd24de164f0e723956dd94fb11
Robert Schade <roscha80@web.de>
2021-05-24 22:12:00 +0200

    Deleted unused import

--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -1,7 +1,6 @@
 import PropTypes from 'prop-types'
 import styled from 'styled-components/macro'
 import Button from '../components/Button'
-import Header from '../components/Header'
 import Player from '../components/Player'
 
 GamePage.propTypes = {
