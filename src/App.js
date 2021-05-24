612a8d01a53a99d8f4f3b11584c84b21c543e4af
Robert Schade <roscha80@web.de>
2021-05-24 22:11:30 +0200

    Cleaned code App.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import CreatePage from './pages/CreatePage'
 import GamePage from './pages/GamePage'
 import styled from 'styled-components/macro'
 import Nav from './components/Nav'
 import HistoryPage from './pages/HistoryPage'
-import { useEffect } from 'react'
 import Header from './components/Header'
 
 export default function App() {
